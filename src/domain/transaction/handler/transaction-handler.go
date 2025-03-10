a942eeae3b8712e721a16899b16c19eb778e1006
Andri Nur Hidayatulloh <andribis13@gmail.com>
2025-03-10 12:00:21 +0700

    refactor transaction handler: remove FindByUserId method and update CreateTransaction to use UserId from request

--- src/domain/transaction/handler/transaction-handler.go
+++ src/domain/transaction/handler/transaction-handler.go
@@ -5,7 +5,6 @@ import (
 	"strconv"
 	"test-kp-golang/src/domain/transaction/request"
 	usecase "test-kp-golang/src/domain/transaction/use-case"
-	"test-kp-golang/src/domain/user/entity"
 	"test-kp-golang/src/utils"
 
 	"github.com/gin-gonic/gin"
@@ -18,32 +17,29 @@ type TransactionHandler struct {
 func NewTransactionHandler(r *gin.Engine, useCase *usecase.TransactionUseCase) {
 	handler := &TransactionHandler{useCase: useCase}
 
-	r.GET("/transactions", handler.FindByUserId)
-	r.GET("/transactions/:id", handler.FindByUserId)
+	// r.GET("/transactions", handler.FindByUserId)
+	r.GET("/transactions/:id", handler.FindById)
 	r.POST("/transactions", handler.CreateTransaction)
 }
 
-func (h *TransactionHandler) FindByUserId(c *gin.Context) {
-	currentUser := c.MustGet("currentUser").(entity.User)
-	transactions, err := h.useCase.FindByUserId(currentUser.ID)
-	if err != nil {
-		utils.FormatErrorResponse(c, "error", http.StatusBadRequest, err.Error(), nil)
-		return
-	}
+// func (h *TransactionHandler) FindByUserId(c *gin.Context) {
+// 	transactions, err := h.useCase.FindByUserId(currentUser.ID)
+// 	if err != nil {
+// 		utils.FormatErrorResponse(c, "error", http.StatusBadRequest, err.Error(), nil)
+// 		return
+// 	}
 
-	utils.FormatErrorResponse(c, "success", http.StatusOK, "success", transactions)
-}
+// 	utils.FormatErrorResponse(c, "success", http.StatusOK, "success", transactions)
+// }
 
 func (h *TransactionHandler) CreateTransaction(c *gin.Context) {
-	currentUser := c.MustGet("currentUser").(entity.User)
-
 	var transaction request.TransactionRequest
 	if err := c.ShouldBindJSON(&transaction); err != nil {
 		utils.FormatErrorResponse(c, "error", http.StatusBadRequest, err.Error(), nil)
 		return
 	}
 
-	transactionResponse, err := h.useCase.Create(currentUser.ID, transaction)
+	transactionResponse, err := h.useCase.Create(transaction.UserId, transaction)
 	if err != nil {
 		utils.FormatErrorResponse(c, "error", http.StatusBadRequest, err.Error(), nil)
 		return
